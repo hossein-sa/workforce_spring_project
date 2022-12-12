5a385f6ce73212a0a785a7a1c2a1bfccb0bdcbbe
Hossein Sadeghi <hossein.sadeghi.official@gmail.com>
2022-12-12 21:51:19 +0330

    Create Spring Boot Project for Workforce

---
+++ src/main/java/sa/aref/workforce_spring_project/WorkforceSpringProjectApplication.java
@@ -0,0 +1,13 @@
+package sa.aref.workforce_spring_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkforceSpringProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkforceSpringProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/sa/aref/workforce_spring_project/WorkforceSpringProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package sa.aref.workforce_spring_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkforceSpringProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
