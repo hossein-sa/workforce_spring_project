e1f7dd74edfac37c2feea1730f50efea33b74f6b
Hossein Sadeghi <hossein.sadeghi.official@gmail.com>
2022-12-12 22:19:59 +0330

    Remove some packages and files bcz mistake

--- src/main/java/sa/aref/WorkforceSpringProjectApplication.java
+++ src/main/java/sa/aref/WorkforceSpringProjectApplication.java
@@ -1,4 +1,4 @@
-package sa.aref.workforce_spring_project;
+package sa.aref;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
