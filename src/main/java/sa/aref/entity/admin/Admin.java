1b100e68b57f5b834be24f5bb44e9fa3818c48a5
Hossein Sadeghi <hossein.sadeghi.official@gmail.com>
2022-12-22 17:02:59 +0330

    add admin entity

---
+++ src/main/java/sa/aref/entity/admin/Admin.java
@@ -0,0 +1,26 @@
+package sa.aref.entity.admin;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Pattern;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+public class Admin {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    @Column(unique = true)
+    private String email;
+    @Pattern(regexp = "^(?=.*\\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$",
+            message = "password must contain at least  1 uppercase, 1 lowercase and 1 digit")
+    private String password;
+
+
+}
