b5735214549fe98c5db3cfbec7e2f751d59fd6b9
Hossein Sadeghi <hossein.sadeghi.official@gmail.com>
2022-12-22 16:49:16 +0330

    add @Modifying to change password method

--- src/main/java/sa/aref/repository/accounts/ExpertRepository.java
+++ src/main/java/sa/aref/repository/accounts/ExpertRepository.java
@@ -1,12 +1,14 @@
 package sa.aref.repository.accounts;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import sa.aref.entity.accounts.ExpertAccount;
 
 @Repository
 public interface ExpertRepository extends JpaRepository<ExpertAccount, Long> {
+@Modifying
 @Query("""
 update ExpertAccount e
 set e.password = :password
