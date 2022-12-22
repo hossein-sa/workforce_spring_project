e7cc1283944276d5750e5a8acb257b313ff3c158
Hossein Sadeghi <hossein.sadeghi.official@gmail.com>
2022-12-22 16:18:27 +0330

    create ClientService and add register method

---
+++ src/main/java/sa/aref/service/accounts/ClientService.java
@@ -0,0 +1,19 @@
+package sa.aref.service.accounts;
+
+import org.springframework.stereotype.Service;
+import sa.aref.entity.accounts.ClientAccount;
+import sa.aref.repository.accounts.ClientRepository;
+
+@Service
+public class ClientService {
+    private final ClientRepository clientRepository;
+
+    public ClientService(ClientRepository clientRepository) {
+        this.clientRepository = clientRepository;
+    }
+
+    public ClientAccount registerClient(ClientAccount client) {
+        return clientRepository.save(client);
+    }
+
+}
