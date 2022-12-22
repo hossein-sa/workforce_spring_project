09c05b9cfdef985c5b128e776951d1ee12df9c24
Hossein Sadeghi <hossein.sadeghi.official@gmail.com>
2022-12-22 16:22:42 +0330

    Register method for ExpertService

--- src/main/java/sa/aref/service/accounts/ExpertService.java
+++ src/main/java/sa/aref/service/accounts/ExpertService.java
@@ -1,6 +1,7 @@
 package sa.aref.service.accounts;
 
 import org.springframework.stereotype.Service;
+import sa.aref.entity.accounts.ExpertAccount;
 import sa.aref.repository.accounts.ExpertRepository;
 
 @Service
@@ -9,5 +10,9 @@ public class ExpertService {
 
     public ExpertService(ExpertRepository expertRepository) {
         this.expertRepository = expertRepository;
+    }
+
+    public ExpertAccount registerExpert(ExpertAccount expert) {
+        return expertRepository.save(expert);
     }
 }
