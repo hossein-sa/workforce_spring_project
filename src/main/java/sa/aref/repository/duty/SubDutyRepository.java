7860b1bb59b2d1049bc91f8e8cca9df094de00d3
Hossein Sadeghi <hossein.sadeghi.official@gmail.com>
2022-12-22 21:50:03 +0330

    add findByMainDutiesId method

--- src/main/java/sa/aref/repository/duty/SubDutyRepository.java
+++ src/main/java/sa/aref/repository/duty/SubDutyRepository.java
@@ -4,6 +4,9 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import sa.aref.entity.duties.SubDuties;
 
+import java.util.List;
+
 @Repository
 public interface SubDutyRepository extends JpaRepository<SubDuties, Long> {
+    List<SubDuties> findByMainDutiesId(Long id);
 }
--- src/main/java/sa/aref/service/duty/SubDutyService.java
+++ src/main/java/sa/aref/service/duty/SubDutyService.java
@@ -4,6 +4,8 @@ import org.springframework.stereotype.Service;
 import sa.aref.entity.duties.SubDuties;
 import sa.aref.repository.duty.SubDutyRepository;
 
+import java.util.List;
+
 @Service
 public class SubDutyService {
     private final SubDutyRepository subDutyRepository;
@@ -14,5 +16,9 @@ public class SubDutyService {
 
     public void addSubDuty(SubDuties subDuty) {
         subDutyRepository.save(subDuty);
+    }
+
+    public List<SubDuties> findByMainDutiesId(Long id) {
+        return subDutyRepository.findByMainDutiesId(id);
     }
 }
