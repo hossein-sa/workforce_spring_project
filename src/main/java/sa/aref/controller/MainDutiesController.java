0611aa1d799610e2b1bdf163e1f310f107e1524f
Hossein Sadeghi <sadeghi.ho@hotmail.com>
2023-01-05 17:10:04 +0330

    Add MainDutiesController

---
+++ src/main/java/sa/aref/controller/MainDutiesController.java
@@ -0,0 +1,33 @@
+package sa.aref.controller;
+
+import org.springframework.data.repository.query.Param;
+import org.springframework.web.bind.annotation.*;
+import sa.aref.entity.duties.MainDuties;
+import sa.aref.service.duty.MainDutyService;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/main-duties")
+public class MainDutiesController {
+    private final MainDutyService mainDutyService;
+
+    public MainDutiesController(MainDutyService mainDutyService) {
+        this.mainDutyService = mainDutyService;
+    }
+
+    @GetMapping("/all")
+    public List<MainDuties> getMainDuties() {
+        return mainDutyService.getMainDuties();
+    }
+
+    @PostMapping("/new/{dutyName}")
+    public String addNewMainDuty(@PathVariable String dutyName) {
+        if (mainDutyService.addMainDuty(MainDuties.builder().name(dutyName).build()))
+            return "OK";
+        else
+            return "Failed";
+    }
+
+
+}
