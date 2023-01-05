e5236e0d6305507bff2879b5fef925fb10397553
Hossein Sadeghi <sadeghi.ho@hotmail.com>
2023-01-05 17:06:55 +0330

    change in SubDuties and MainDuties entity

--- src/main/java/sa/aref/entity/duties/MainDuties.java
+++ src/main/java/sa/aref/entity/duties/MainDuties.java
@@ -1,15 +1,21 @@
 package sa.aref.entity.duties;
 
 import jakarta.persistence.*;
+import lombok.*;
 
 import java.util.Set;
 
 @Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 public class MainDuties {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+    private Integer id;
     private String name;
     @OneToMany(mappedBy = "mainDuties")
-    private Set<SubDuties> duties;
+    private Set<SubDuties> subDuties;
 }
--- src/main/java/sa/aref/entity/duties/SubDuties.java
+++ src/main/java/sa/aref/entity/duties/SubDuties.java
@@ -1,23 +1,30 @@
 package sa.aref.entity.duties;
 
 import jakarta.persistence.*;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
+import sa.aref.entity.accounts.ExpertAccount;
+
+import java.util.LinkedHashSet;
+import java.util.Set;
 
 @Entity
 @Getter
 @Setter
 @NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class SubDuties {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+    private Integer id;
     @Column(unique = true)
     private String name;
-    private Double price;
+    private Long price;
     private String description;
     @ManyToOne
     private MainDuties mainDuties;
 
+
+    @ManyToMany
+    private Set<ExpertAccount> expertAccounts;
 }
