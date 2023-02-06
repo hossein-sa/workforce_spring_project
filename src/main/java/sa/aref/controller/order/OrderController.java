8f32d7cebd6a575266e011465ed09939b6214f4e
Hossein Sadeghi <hossein.sadeghi.official@gmail.com>
2023-02-06 22:54:06 +0330

    add find all method to OrderController

--- src/main/java/sa/aref/controller/order/OrderController.java
+++ src/main/java/sa/aref/controller/order/OrderController.java
@@ -1,8 +1,12 @@
 package sa.aref.controller.order;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import sa.aref.entity.order.Order;
 import sa.aref.service.order.OrderService;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/order")
@@ -11,5 +15,9 @@ public class OrderController {
 
     public OrderController(OrderService orderService) {
         this.orderService = orderService;
+    }
+    @GetMapping("/all")
+    public List<Order> getOrder() {
+        return orderService.getOrders();
     }
 }
