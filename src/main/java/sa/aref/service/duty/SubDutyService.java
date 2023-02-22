5a4f9e0dc0a3039bf06a142e57aadbabe45ed4b8
Hossein Sadeghi <hossein.sadeghi.offcial@gmail.com>
2023-02-22 12:21:22 +0330

    add addExpertToSubDuties To SubDutyService

--- src/main/java/sa/aref/service/duty/SubDutyService.java
+++ src/main/java/sa/aref/service/duty/SubDutyService.java
@@ -1,18 +1,23 @@
 package sa.aref.service.duty;
 
 import org.springframework.stereotype.Service;
+import sa.aref.entity.accounts.ExpertAccount;
 import sa.aref.entity.duties.SubDuties;
 import sa.aref.exception.CustomExceptionIsExist;
+import sa.aref.exception.CustomExceptionNotFound;
+import sa.aref.repository.accounts.ExpertRepository;
 import sa.aref.repository.duty.SubDutyRepository;
 
 @Service
 public class SubDutyService {
     private final SubDutyRepository subDutyRepository;
     private final MainDutyService mainDutyService;
+    private final ExpertRepository expertRepository;
 
-    public SubDutyService(SubDutyRepository subDutyRepository, MainDutyService mainDutyService) {
+    public SubDutyService(SubDutyRepository subDutyRepository, MainDutyService mainDutyService, ExpertRepository expertRepository) {
         this.subDutyRepository = subDutyRepository;
         this.mainDutyService = mainDutyService;
+        this.expertRepository = expertRepository;
     }
 
 
@@ -22,6 +27,15 @@ public class SubDutyService {
         if (!mainDutyService.existsByName(subDuty.getMainDuties().getName()))
             throw new CustomExceptionIsExist("Main Duty is not exist");
         subDutyRepository.save(subDuty);
+    }
+
+    public void addExpertToSubDuties(Integer subDutiesId, Integer expertId) {
+        SubDuties subDuties = subDutyRepository.findById(subDutiesId)
+                .orElseThrow(() -> new CustomExceptionNotFound("SubDuties not found with id: " + subDutiesId));
+        ExpertAccount expert = expertRepository.findById(expertId)
+                .orElseThrow(() -> new CustomExceptionNotFound("ExpertAccount not found with id: " + expertId));
+        subDuties.getExpertAccounts().add(expert);
+        subDutyRepository.save(subDuties);
     }
 
 //    public List<SubDuties> findByMainDutiesId(Long id) {
